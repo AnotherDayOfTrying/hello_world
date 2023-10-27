2103380d4ee5523ab6c3a879a7c5ec2f0ff1da7f
Tobrown3 <saifhusnain@gmail.com>
2023-10-27 13:11:44 -0600

    Added Friend request and accept, decline, backend

--- backend/hello_world_rest_api/authors/urls.py
+++ backend/hello_world_rest_api/authors/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 app_name = 'authors'
 urlpatterns = [
     path('signup/', views.Signup.as_view(), name = 'signup'),
-    path('signin/', views.Signin.as_view(), name = 'signin')
+    path('signin/', views.Signin.as_view(), name = 'signin'),
+    path('frequests/send/', views.SendFriendRequest.as_view(), name = 'send-friend-request'),
+    path('frequests/respond/<int:friendship_id>/', views.FriendRequestResponse.as_view(), name = 'respond-friend-request')
 ]

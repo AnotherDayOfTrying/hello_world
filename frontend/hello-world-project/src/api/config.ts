3ae042493704741fe7440007ef2794dfdd5eb2d9
justin javier <javierjustin7@gmail.com>
2023-10-28 20:44:47 -0600

    test: debugging

--- frontend/hello-world-project/src/api/config.ts
+++ frontend/hello-world-project/src/api/config.ts
@@ -4,4 +4,6 @@ const APIURL: any = {
     "prod": "https://cmput404-project-backend-a299a47993fd.herokuapp.com",
 }
 
+console.log(JSON.stringify(process.env, null, 2))
+
 export default APIURL[process.env.ENV || "dev"];

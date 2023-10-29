d59f01c91895fbb519603f8435f2031ec559662c
Tobrown3 <saifhusnain@gmail.com>
2023-10-28 20:01:42 -0600

    Bckend allowing for creating comments on posts

--- backend/hello_world_rest_api/authors/urls.py
+++ backend/hello_world_rest_api/authors/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('signup/', views.Signup.as_view(), name = 'signup'),
     path('signin/', views.Signin.as_view(), name = 'signin'),
     path('frequests/send/', views.SendFriendRequest.as_view(), name = 'send-friend-request'),
-    path('frequests/respond/<int:friendship_id>/', views.FriendRequestResponse.as_view(), name = 'respond-friend-request')
+    path('frequests/respond/<int:friendship_id>/', views.FriendRequestResponse.as_view(), name = 'respond-friend-request'),
+    path('comments/<int:post_id>/', views.PostComment.as_view(), name = 'post-comment'),
 ]

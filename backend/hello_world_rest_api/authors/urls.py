e34218fe0c78c7f8cb0bbf2778566af6f4a6750f
CKP3operator <perezcharles1112@gmail.com>
2023-12-08 13:08:26 -0700

    changed the view for post/post id/image

--- backend/hello_world_rest_api/authors/urls.py
+++ backend/hello_world_rest_api/authors/urls.py
@@ -20,6 +20,4 @@ urlpatterns = [
     path('authors/<uuid:author_id>/requests/', views.FollowRequestView.as_view(), name='getfollowrequests'),
     path('authors/<uuid:author_id>/friends/', views.FriendsView.as_view(), name='getfriends'),
     path('authors/node/', views.SetupNode.as_view(), name='getnodeauthors'),
-    path('nodes/', views.GetNodesView.as_view(), name='getnodes'),
-    path('posts/', views.GetPublicPost.as_view(), name = 'getallposts'),
 ]
